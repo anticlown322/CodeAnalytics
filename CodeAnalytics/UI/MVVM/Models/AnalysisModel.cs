cc65eedc8952352d017e9001891acbf0ed72ea8a
anticlown322 <karasandrey2005@gmail.com>
2024-10-26 11:16:31 +0300

    refactor + feat
    - rename coretypes to HalsterCoreTypes
    - create Jilba assembly

--- CodeAnalytics/UI/MVVM/Models/AnalysisModel.cs
+++ CodeAnalytics/UI/MVVM/Models/AnalysisModel.cs
@@ -79,10 +79,10 @@ public class AnalysisModel : INotifyPropertyChanged
     public void StartCodeAnalysis(string codeToAnalyze)
     {
         _lexer = new Lexer(codeToAnalyze);
-        
-        OperatorsSet = _lexer.GetOperatorsSet();
+
         OperandsSet = _lexer.GetOperandsSet();
-        
+        OperatorsSet = _lexer.GetOperatorsSet();
+
         (int, int, int) additionalMetrics = _lexer.GetTextParams(OperandsSet, OperatorsSet);
 
         ProgramDictionary = additionalMetrics.Item1;
